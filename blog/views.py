ac518877c9fbe9fddaac740448c161b697c7de2e
0laolu <abdullateefolaolu@gmail.com>
2022-06-29 11:57:55 +0100

    create class based views in the views file of the `blog` app

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
+from django.urls import reverse_lazy
+from .models import Post
 
 # Create your views here.
+
+class PostListView(ListView):
+    model = Post
+    template_name: 'post_list.html'
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields: "__all__"
+    success_url = reverse_lazy("blog:all")
+    template_name: 'base.html'
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name: 'post_detail.html'
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields: "__all__"
+    success_url = reverse_lazy("blog:all")
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    fields: "__all__"
+    success_url = reverse_lazy("blog:all")
+    template_name = 'post_confirm_delete.html'
+
